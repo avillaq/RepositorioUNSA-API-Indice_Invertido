565d910374c3f561b67c8a52065d5f4949c6e03e
avillaq <villafuertequispealex@gmail.com>
2024-12-03 17:15:01 -0500

    Add dynamic Swagger JSON processing and redirect root to API endpoint

--- app/swagger.py
+++ app/swagger.py
@@ -1,10 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from flask_swagger_ui import get_swaggerui_blueprint
+import json
+import os
 
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
 
-swagger_bp = Blueprint('swagger', __name__)
+# Función para modificar el swagger.json en tiempo de ejecución
+def swagger_json_processor():
+    with open('app/static/swagger.json', 'r') as file:
+        swagger_data = json.load(file)
+        # Obtener el host dinámicamente
+        swagger_data['host'] = request.host
+        # Actualizar schemes según el protocolo
+        swagger_data['schemes'] = ['https' if request.is_secure else 'http']
+        return swagger_data
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
@@ -13,3 +23,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
         'app_name': "Repositorio UNSA API"
     }
 )
+
+@swaggerui_blueprint.route('/static/swagger.json')
+def serve_swagger_json():
+    return jsonify(swagger_json_processor())
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
 from app import create_app
+from flask import redirect
 
 app = create_app()
+
+@app.route('/')
+def index_api():
+    return redirect('/api')
